a5a5d452f589fef1b27c922a9f430033a867d396
Jadson Butzk <84212592+JadsonButzk@users.noreply.github.com>
2021-12-23 10:58:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function valorparaimpostosnoano(){
-    var salario=document.getElementById("salariomensaldotrabalhador")
-    salario=Number(salario.value)
-    valorbrutototalnoano=salario*13
-    vbtnaemreal=valorbrutototalnoano.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})
-    valorparaimpostos=(salario/31)*149
-    vpiemreal=valorparaimpostos.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})
-    alert(`O valor para impostos, em aproximação, corresponde a ${vpiemreal} sendo ${vbtnaemreal} o salário anual considerando o 13º.`)
-}
